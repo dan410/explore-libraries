3db890a84c5352908725b884a4a51c400250e278
Fortin, Daniel C <daniel.fortin@pnnl.gov>
2018-10-08 15:25:43 -0700

    create data sets describing R packages

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -14,15 +14,8 @@ pk <- as_data_frame(installed.packages())
 ##   * Priority
 ##   * Built
 
-pk <- select(pk, Package, LibPath, Version, Priority, Built)
+pk <- dplyr::select(pk, Package, LibPath, Version, Priority, Built)
 
 ## write this data frame to data/installed-packages.csv
-## hint: readr::write_csv() or write.table()
-## idea: try using here::here() to create the file path
-
 readr::write_csv(pk, here("data", "installed-packages.csv"))
 
-
-## YES overwrite the file that is there now (or delete it first)
-## that came from me (Jenny)
-## it an example of what yours should look like and where it should go
